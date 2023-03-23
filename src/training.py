881f63525f0fad8bcaf7e88a7f3e93c35a8bbf87
tanthinhdt <tanthinhdt24@gmail.com>
2023-03-24 00:48:54 +0700

    update training

--- src/training.py
+++ src/training.py
@@ -4,41 +4,84 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as tforms
 import torchvision.datasets as dtsets
+import matplotlib.pyplot as plt
 from tqdm import tqdm
+from torchsummary import summary
 
 
 class Training():
-    def __init__(self, data_dir, ckpt_dir, train_ratio, interpolate, 
-                 img_size, model, optimizer, scheduler=None):
+    def __init__(self, data_dir, train_ratio, interpolate, ckpt_dir,
+                 from_ckpt, model_ptt, model_params,
+                 optimizer_ptt, optimizer_params,
+                 scheduler_ptt, scheduler_params):
         self.device = torch.device(
             'cuda' if torch.cuda.is_available() else 'cpu')
 
         self.data_dir = data_dir
-        self.ckpt_dir = ckpt_dir
         self.train_ratio = train_ratio
         self.interpolate = interpolate
-        self.img_size = img_size
+        self.ckpt_dir = ckpt_dir
 
-        self.model = model.to(self.device)
-        self.optimizer = optimizer
-        self.scheduler = scheduler
+        checkpoint = None
+        if from_ckpt:
+            ckpt_path = os.path.join(self.ckpt_dir, 'checkpoint.pth')
+            checkpoint = torch.load(ckpt_path)
+
+        self.model_params = model_params
+        self.model = self.get_model(model_ptt=model_ptt,
+                                    model_params=model_params,
+                                    checkpoint=checkpoint).to(self.device)
+        self.optimizer = self.get_optimizer(optimizer_ptt=optimizer_ptt,
+                                            optimizer_params=optimizer_params,
+                                            checkpoint=checkpoint)
+        if scheduler_ptt and scheduler_params:
+            self.scheduler = self.get_scheduler(scheduler_ptt=scheduler_ptt,
+                                                scheduler_params=scheduler_params,
+                                                checkpoint=checkpoint)
+        else:
+            self.scheduler = None
 
         self.train_history = {
             'train_loss': [],
             'val_acc': []
         }
 
+    def get_model(self, model_ptt, model_params, checkpoint=None):
+        model = model_ptt(**model_params)
+        if checkpoint:
+            model.load_state_dict(checkpoint['model_state_dict'])
+        return model
+
+    def get_model_summary(self):
+        input_shape = (self.model_params['img_channel'],
+                       self.model_params['img_size'],
+                       self.model_params['img_size'])
+        return summary(self.model, input_shape)
+
+    def get_optimizer(self, optimizer_ptt, optimizer_params, checkpoint=None):
+        optimizer = optimizer_ptt(params=self.model.parameters(),
+                                  **optimizer_params)
+        if checkpoint:
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        return optimizer
+
+    def get_scheduler(self, scheduler_ptt, scheduler_params, checkpoint=None):
+        scheduler = scheduler_ptt(optimizer=self.optimizer, **scheduler_params)
+        if checkpoint:
+            scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+        return scheduler
+
     def load_data(self):
+        transforms = tforms.Compose([
+            tforms.ToTensor(),
+            # tforms.Normalize(mean=(0.5,), std=(0.5,)),
+            # tforms.Normalize(mean=(0.1307,), std=(0.3081,)),
+            tforms.Lambda(lambd=lambda X: X / 255)
+        ])
         if self.interpolate:
             transforms = tforms.Compose([
-                tforms.ToTensor(),
-                tforms.Resize((32, 32)),
-                tforms.Normalize(mean=(0.5,), std=(0.5,)),
-            ])
-        else:
-            transforms = tforms.Compose([
-                tforms.ToTensor(),
-                tforms.Normalize(mean=(0.5,), std=(0.5,)),
+                tforms.Resize((self.interpolate, self.interpolate)),
+                transforms
             ])
 
         train_set = dtsets.FashionMNIST(
@@ -51,23 +94,25 @@ class Training():
 
         return train_set, val_set
 
-    def get_data_loader(self, batch_sizes, shuffle):
-        datasets = self.load_data()
-        data_loaders = []
-        for dataset, batch_size in zip(datasets, batch_sizes):
-            data_loader = torch.utils.data.DataLoader(dataset=dataset,
-                                                      batch_size=batch_size,
-                                                      shuffle=shuffle)
-            data_loaders.append(data_loader)
-        return data_loaders
+    def get_data_loader(self, batch_sizes):
+        train_set, val_set = self.load_data()
+        train_loader = torch.utils.data.DataLoader(dataset=train_set,
+                                                   batch_size=batch_sizes[0],
+                                                   shuffle=True)
+        val_loader = torch.utils.data.DataLoader(dataset=val_set,
+                                                 batch_size=batch_sizes[1],
+                                                 shuffle=False)
+        return train_loader, val_loader
 
-    def train(self, epochs, save_every, batch_sizes, shuffle):
+    def train(self, epochs, save_every, batch_sizes):
         criterion = nn.CrossEntropyLoss()
-        train_loader, val_loader = self.get_data_loader(batch_sizes=batch_sizes,
-                                                        shuffle=shuffle)
+        train_loader, val_loader = self.get_data_loader(
+            batch_sizes=batch_sizes)
 
         for epoch in tqdm(range(epochs), desc='Epochs', total=epochs, leave=False,
                           unit='epoch'):
+            batch_loss = 0
+            count = 0
             for X, y in tqdm(train_loader, desc='Sample per batch',
                              total=batch_sizes[0], leave=False):
                 self.model.train()
@@ -79,12 +124,16 @@ class Training():
                 self.optimizer.step()
                 if self.scheduler:
                     self.scheduler.step()
-                self.train_history['train_loss'].append(loss.data.item())
+                batch_loss += loss.data.item()
+                count += 1
+            self.train_history['train_loss'].append(batch_loss / count)
 
-            self.train_history['val_acc'].append(self.evaluate(val_loader=val_loader))
+            self.model.eval()
+            self.train_history['val_acc'].append(
+                self.evaluate(val_loader=val_loader))
 
-            if epoch % save_every == 0:
-                self.save(epoch=epoch)
+            if (epoch + 1) % save_every == 0:
+                self.save()
 
         return self.train_history
 
@@ -92,15 +141,15 @@ class Training():
         correct = 0
         total = 0
         for X, y in val_loader:
-            total += 1
             X, y = X.to(self.device), y.to(self.device)
             y_hat = self.model(X)
-            _, label = torch.max(y_hat, 1)
-            correct += (label == y).sum().item()
+            _, pred = torch.max(y_hat, 1)
+            correct += (pred == y).sum().item()
+            total += len(X)
         return correct / total
 
-    def save(self, epoch):
-        ckpt_path = os.path.join(self.ckpt_dir, f'checkpoint_{epoch}.pth')
+    def save(self):
+        ckpt_path = os.path.join(self.ckpt_dir, 'checkpoint.pth')
         if self.scheduler:
             torch.save({
                 'model_state_dict': self.model.state_dict(),
@@ -113,3 +162,21 @@ class Training():
                 'optimizer_state_dict': self.optimizer.state_dict(),
                 'scheduler_state_dict': None
             }, ckpt_path)
+
+    def plot_results(self):
+        fig, ax1 = plt.subplots()
+        color = 'tab:red'
+        ax1.plot(self.train_history['train_loss'], color=color)
+        ax1.set_xlabel('epoch', color=color)
+        ax1.set_ylabel('total loss', color=color)
+        ax1.tick_params(axis='y', color=color)
+
+        ax2 = ax1.twinx()
+        color = 'tab:blue'
+        ax2.set_ylabel('accuracy', color=color)
+        ax2.plot(self.train_history['val_acc'], color=color)
+        ax2.tick_params(axis='y', labelcolor=color)
+        ax2.tick_params(axis='y', labelcolor=color)
+        fig.tight_layout()
+
+        plt.show()
