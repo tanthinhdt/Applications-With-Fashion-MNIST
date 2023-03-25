cf6bda62f51758ebe632d82eb125b72e20f8cdc7
tanthinhdt <tanthinhdt24@gmail.com>
2023-03-25 14:45:20 +0700

    optimize code

--- models/vgg/vgg19.py
+++ models/vgg/vgg19.py
@@ -16,88 +16,88 @@ class VGG19(nn.Module):
         # first 2 convolutional layers with
         # 64 filters, kernel size 3x3, stride 1, padding 1, ReLU
         cnn_block1_channels = [self.img_channel, 64, 64]
-        cnn_block1_kernel_sizes = [3, 3]
-        cnn_block1_strides = [1, 1]
-        cnn_block1_paddings = [1, 1]
+        cnn_block1_kernel_sizes = [3]
+        cnn_block1_strides = [1]
+        cnn_block1_paddings = [1]
         self.features.extend(CNNBlock(channels=cnn_block1_channels,
                                       kernel_sizes=cnn_block1_kernel_sizes,
                                       stride=cnn_block1_strides,
                                       padding=cnn_block1_paddings).get_block())
 
-        # 1 max pooling layer with kernel size 2x2, stride 2
+        # max pooling layer with kernel size 2x2, stride 2
         self.features.append(nn.MaxPool2d(kernel_size=2, stride=2))
         img_size = (img_size - 2) // 2 + 1
 
         # 2 convolutional layers with
         # 128 filters, kernel size 3x3, stride 1, padding 1, ReLU
         cnn_block2_channels = [cnn_block1_channels[-1], 128, 128]
-        cnn_block2_kernel_sizes = [3, 3]
-        cnn_block2_strides = [1, 1]
-        cnn_block2_paddings = [1, 1]
+        cnn_block2_kernel_sizes = [3]
+        cnn_block2_strides = [1]
+        cnn_block2_paddings = [1]
         self.features.extend(CNNBlock(channels=cnn_block2_channels,
                                       kernel_sizes=cnn_block2_kernel_sizes,
                                       stride=cnn_block2_strides,
                                       padding=cnn_block2_paddings).get_block())
 
-        # 1 max pooling layer with kernel size 2x2, stride 2
+        # max pooling layer with kernel size 2x2, stride 2
         self.features.append(nn.MaxPool2d(kernel_size=2, stride=2))
         img_size = (img_size - 2) // 2 + 1
 
         # 4 convolutional layers with
         # 256 filters, kernel size 3x3, stride 1, padding 1, ReLU
         cnn_block3_channels = [cnn_block2_channels[-1], 256, 256, 256, 256]
-        cnn_block3_kernel_sizes = [3, 3, 3, 3]
-        cnn_block3_strides = [1, 1, 1, 1]
-        cnn_block3_paddings = [1, 1, 1, 1]
+        cnn_block3_kernel_sizes = [3]
+        cnn_block3_strides = [1]
+        cnn_block3_paddings = [1]
         self.features.extend(CNNBlock(channels=cnn_block3_channels,
                                       kernel_sizes=cnn_block3_kernel_sizes,
                                       stride=cnn_block3_strides,
                                       padding=cnn_block3_paddings).get_block())
 
-        # 1 max pooling layer with kernel size 2x2, stride 2
+        # max pooling layer with kernel size 2x2, stride 2
         self.features.append(nn.MaxPool2d(kernel_size=2, stride=2))
         img_size = (img_size - 2) // 2 + 1
 
         # 4 convolutional layers with
         # 512 filters, kernel size 3x3, stride 1, padding 1, ReLU
         cnn_block4_channels = [cnn_block3_channels[-1], 512, 512, 512, 512]
-        cnn_block4_kernel_sizes = [3, 3, 3, 3]
-        cnn_block4_strides = [1, 1, 1, 1]
-        cnn_block4_paddings = [1, 1, 1, 1]
+        cnn_block4_kernel_sizes = [3]
+        cnn_block4_strides = [1]
+        cnn_block4_paddings = [1]
         self.features.extend(CNNBlock(channels=cnn_block4_channels,
                                       kernel_sizes=cnn_block4_kernel_sizes,
                                       stride=cnn_block4_strides,
                                       padding=cnn_block4_paddings).get_block())
 
-        # 1 max pooling layer with kernel size 2x2, stride 2
+        # max pooling layer with kernel size 2x2, stride 2
         self.features.append(nn.MaxPool2d(kernel_size=2, stride=2))
         img_size = (img_size - 2) // 2 + 1
 
         # 4 convolutional layers with
         # 512 filters, kernel size 3x3, stride 1, padding 1, ReLU
         cnn_block5_channels = [cnn_block4_channels[-1], 512, 512, 512, 512]
-        cnn_block5_kernel_sizes = [3, 3, 3, 3]
-        cnn_block5_strides = [1, 1, 1, 1]
-        cnn_block5_paddings = [1, 1, 1, 1]
+        cnn_block5_kernel_sizes = [3]
+        cnn_block5_strides = [1]
+        cnn_block5_paddings = [1]
         self.features.extend(CNNBlock(channels=cnn_block5_channels,
                                       kernel_sizes=cnn_block5_kernel_sizes,
                                       stride=cnn_block5_strides,
                                       padding=cnn_block5_paddings).get_block())
 
-        # 1 max pooling layer with kernel size 2x2, stride 2
+        # max pooling layer with kernel size 2x2, stride 2
         self.features.append(nn.MaxPool2d(kernel_size=2, stride=2))
         img_size = (img_size - 2) // 2 + 1
 
-        # 1 flatten layer
+        # flatten layer
         self.features.append(nn.Flatten())
 
-        # 2 fully connected layer with 4096 units, ReLu activation
+        # 2 fully connected layers with 4096 units, ReLu activation
         dense_block_dims = [img_size**2 * 512, 4096, 4096]
         self.classifier.extend(DenseBlock(dims=dense_block_dims,
-                                          activation='relu',
-                                          dropout=0.5).get_block())
+                                          activations=['relu'],
+                                          dropouts=[0.5]).get_block())
 
-        # 1 fully connected layer with n_classes units, softmax activation
+        # softmax layer
         self.classifier.append(nn.Linear(dense_block_dims[-1], n_classes))
         self.classifier.append(nn.Softmax(dim=1))
 
